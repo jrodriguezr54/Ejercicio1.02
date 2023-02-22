f116934c38e8672943017c8105121ad7208ac882
josue <josue@DESKTOP-EAEGK61>
2023-02-21 21:31:15 -0600

    Agregar archivos de proyecto.

---
+++ Ejercicio1.02/Ejercicio1.02/Page1.xaml.cs
@@ -0,0 +1,36 @@
+﻿using Ejercicio1._02.models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.PlatformConfiguration.iOSSpecific;
+
+namespace Ejercicio1._02
+{
+    public partial class Page1 : ContentPage
+    {
+        public Page1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnenviar_Clicked(object sender, EventArgs e)
+        {
+            var emple = new empleado
+            {
+                nombre = txtnombre.Text,
+                apellidos = txtapellidos.Text,
+                edad=txtedad.Text,
+                correo=txtcorreo.Text,
+
+            };
+
+            var secondpage = new Pageresultado();
+            secondpage.BindingContext = emple;
+            Navigation.PushAsync(secondpage);
+        }
+    }
+}
---
+++ Ejercicio1.02/Ejercicio1.02/models/empleado.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ejercicio1._02.models
+{
+    class empleado
+    {
+
+        public string nombre { get; set; }
+
+        public string apellidos { get; set;}
+
+        public string edad { get; set;}
+
+        public string correo { get; set; }
+    }
+}
